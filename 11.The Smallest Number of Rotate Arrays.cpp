f80a37b3557931d18b271967fe2b9990b7c7c6c8
VoidShooter <33886139+VoidShooter@users.noreply.github.com>
2019-02-28 20:57:24 +0800

    Update 11.The Smallest Number of Rotate Arrays.cpp
    for the case of 10111

--- 11.The Smallest Number of Rotate Arrays.cpp
+++ 11.The Smallest Number of Rotate Arrays.cpp
@@ -9,14 +9,24 @@ public:
         int left = 0, right = rotateArray.size() - 1, center;
         while(left + 1 != right){
             center = ((right - left)>>1) + left;
-            if(rotateArray[center]>=rotateArray[left]) left = center;
+            if(rotateArray[left]==rotateArray[right]&&rotateArray[left]==rotateArray[center]) return MinInOrder(rotateArray, left, right);
+            else if(rotateArray[center]>=rotateArray[left]) left = center;
             else if(rotateArray[center]<=rotateArray[right]) right = center;
         }
         return rotateArray[right];
     }
+
+private:
+    int MinInOrder(vector<int> rotateArray, int left, int right){
+        int result=rotateArray[left];
+        for(int i=left+1;i<=right;i++){
+            if (rotateArray[i]<result) result = rotateArray[i];
+        }
+        return result;
+    }
 };
 int main() {
-    vector<int> array={3,4,5,1,2};
+    vector<int> array={1,0,1,1,1};
     cout<<Solution().minNumberInRotateArray(array);
     return 0;
 }
